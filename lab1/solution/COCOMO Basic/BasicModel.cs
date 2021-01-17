215c17cb76bbdacc7871a31f5a1ab587ba38735b
Entegrio-tech <entegrioyung@gmail.com>
2021-01-17 21:53:55 +0200

    Экспресс-исправление

--- lab1/solution/COCOMO Basic/BasicModel.cs
+++ lab1/solution/COCOMO Basic/BasicModel.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace COCOMO_Basic
 {
-    class BasicModel
+    internal class BasicModel
     {
         //Таблица коефицывфентов.
-        static double[][] modelTable = new double[3][];
+        private static double[][] modelTable = new double[3][];
+
         static BasicModel()
         {
             modelTable[0] = new[] { 2.4, 1.05, 2.5, 0.38 };
@@ -61,4 +59,3 @@ namespace COCOMO_Basic
         }
     }
 }
-
