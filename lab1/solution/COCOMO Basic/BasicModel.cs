5e8f878bd9ccb65602c07db7764da3e6d4c68511
Entegrio-tech <72520235+Entegrio-tech@users.noreply.github.com>
2021-01-17 17:23:44 +0200

    Задизайнена основная форма приложения

---
+++ lab1/solution/COCOMO Basic/BasicModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COCOMO_Basic
+{
+    class BasicModel
+    {
+        //Таблица коефицывфентов.
+        static double[][] modelTable = new double[3][];
+        static BasicModel()
+        {
+            modelTable[0] = new[] { 2.4, 1.05, 2.5, 0.38 };
+            modelTable[1] = new[] { 3, 1.12, 2.5, 0.35 };
+            modelTable[2] = new[] { 3.6, 1.2, 2.5, 0.32 };
+        }
+    }
+}
