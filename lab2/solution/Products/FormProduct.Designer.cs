9f3136ee6641da69871205e10782d5c8ae73fbcb
Entegrio-tech <entegrioyung@gmail.com>
2021-01-25 19:23:04 +0200

    Основа проекта

---
+++ lab2/solution/Products/FormProduct.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace Products
+{
+    partial class FormProduct
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCipher = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.tbGenre = new System.Windows.Forms.TextBox();
+            this.tbSurcharge = new System.Windows.Forms.TextBox();
+            this.cmbGenre = new System.Windows.Forms.ComboBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnBefore = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbProducts = new System.Windows.Forms.Label();
+            this.lblCipher = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblSurcharge = new System.Windows.Forms.Label();
+            this.tbType = new System.Windows.Forms.TextBox();
+            this.tblNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbCipher
+            // 
+            this.tbCipher.Location = new System.Drawing.Point(78, 141);
+            this.tbCipher.Name = "tbCipher";
+            this.tbCipher.Size = new System.Drawing.Size(51, 20);
+            this.tbCipher.TabIndex = 0;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(135, 141);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(259, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(306, 215);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(88, 20);
+            this.tbPrice.TabIndex = 4;
+            // 
+            // tbGenre
+            // 
+            this.tbGenre.Location = new System.Drawing.Point(468, 141);
+            this.tbGenre.Name = "tbGenre";
+            this.tbGenre.Size = new System.Drawing.Size(38, 20);
+            this.tbGenre.TabIndex = 5;
+            // 
+            // tbSurcharge
+            // 
+            this.tbSurcharge.Location = new System.Drawing.Point(441, 215);
+            this.tbSurcharge.Name = "tbSurcharge";
+            this.tbSurcharge.Size = new System.Drawing.Size(65, 20);
+            this.tbSurcharge.TabIndex = 6;
+            // 
+            // cmbGenre
+            // 
+            this.cmbGenre.FormattingEnabled = true;
+            this.cmbGenre.Location = new System.Drawing.Point(529, 140);
+            this.cmbGenre.Name = "cmbGenre";
+            this.cmbGenre.Size = new System.Drawing.Size(121, 21);
+            this.cmbGenre.TabIndex = 7;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(23, 308);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 8;
+            this.btnFirst.Text = "Первая";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnBefore
+            // 
+            this.btnBefore.Location = new System.Drawing.Point(104, 308);
+            this.btnBefore.Name = "btnBefore";
+            this.btnBefore.Size = new System.Drawing.Size(75, 23);
+            this.btnBefore.TabIndex = 9;
+            this.btnBefore.Text = "Пред.";
+            this.btnBefore.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(185, 308);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "След.";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(266, 308);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "Посл.";
+            this.btnLast.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(347, 308);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 12;
+            this.btnEdit.Text = "Изменить";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(428, 308);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(509, 308);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(590, 308);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(72, 23);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lbProducts
+            // 
+            this.lbProducts.AutoSize = true;
+            this.lbProducts.Location = new System.Drawing.Point(20, 45);
+            this.lbProducts.Name = "lbProducts";
+            this.lbProducts.Size = new System.Drawing.Size(38, 13);
+            this.lbProducts.TabIndex = 16;
+            this.lbProducts.Text = "Товар";
+            // 
+            // lblCipher
+            // 
+            this.lblCipher.AutoSize = true;
+            this.lblCipher.Location = new System.Drawing.Point(75, 125);
+            this.lblCipher.Name = "lblCipher";
+            this.lblCipher.Size = new System.Drawing.Size(39, 13);
+            this.lblCipher.TabIndex = 17;
+            this.lblCipher.Text = "Шифр:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(140, 125);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(92, 13);
+            this.lblName.TabIndex = 18;
+            this.lblName.Text = "Наименнование:";
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Location = new System.Drawing.Point(465, 124);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(39, 13);
+            this.lblGenre.TabIndex = 19;
+            this.lblGenre.Text = "Жанр:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoEllipsis = true;
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(75, 199);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(29, 13);
+            this.lblType.TabIndex = 20;
+            this.lblType.Text = "Тип:";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoEllipsis = true;
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(188, 199);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(70, 13);
+            this.lblNumber.TabIndex = 21;
+            this.lblNumber.Text = "Кол-во в уп.:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoEllipsis = true;
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(305, 199);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(36, 13);
+            this.lblPrice.TabIndex = 22;
+            this.lblPrice.Text = "Цена:";
+            // 
+            // lblSurcharge
+            // 
+            this.lblSurcharge.AutoEllipsis = true;
+            this.lblSurcharge.AutoSize = true;
+            this.lblSurcharge.Location = new System.Drawing.Point(438, 199);
+            this.lblSurcharge.Name = "lblSurcharge";
+            this.lblSurcharge.Size = new System.Drawing.Size(69, 13);
+            this.lblSurcharge.TabIndex = 23;
+            this.lblSurcharge.Text = "% надбавка:";
+            // 
+            // tbType
+            // 
+            this.tbType.Location = new System.Drawing.Point(78, 215);
+            this.tbType.Name = "tbType";
+            this.tbType.Size = new System.Drawing.Size(79, 20);
+            this.tbType.TabIndex = 24;
+            // 
+            // tblNumber
+            // 
+            this.tblNumber.Location = new System.Drawing.Point(191, 215);
+            this.tblNumber.Name = "tblNumber";
+            this.tblNumber.Size = new System.Drawing.Size(43, 20);
+            this.tblNumber.TabIndex = 25;
+            // 
+            // FormProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 405);
+            this.Controls.Add(this.tblNumber);
+            this.Controls.Add(this.tbType);
+            this.Controls.Add(this.lblSurcharge);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblCipher);
+            this.Controls.Add(this.lbProducts);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBefore);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.cmbGenre);
+            this.Controls.Add(this.tbSurcharge);
+            this.Controls.Add(this.tbGenre);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbCipher);
+            this.Name = "FormProduct";
+            this.Text = "Товар";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbCipher;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.TextBox tbGenre;
+        private System.Windows.Forms.TextBox tbSurcharge;
+        private System.Windows.Forms.ComboBox cmbGenre;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnBefore;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbProducts;
+        private System.Windows.Forms.Label lblCipher;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblSurcharge;
+        private System.Windows.Forms.TextBox tbType;
+        private System.Windows.Forms.TextBox tblNumber;
+    }
+}
+
