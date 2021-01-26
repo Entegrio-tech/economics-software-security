9df8933bb82cbd166272ab97fac8bf45465aaf95
Entegrio-tech <entegrioyung@gmail.com>
2021-01-27 01:09:43 +0200

    Исправлена поведение CMB

--- lab2/solution/Products/FormProduct.cs
+++ lab2/solution/Products/FormProduct.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Windows.Forms;
-
 
 namespace Products
 {
     public partial class FormProduct : Form
     {
-        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
         public FormProduct()
         {
             InitializeComponent();
         }
+
         // Событие для начертания линий на форме.
         private void FormProduct_Paint(object sender, PaintEventArgs e)
         {
@@ -26,11 +24,13 @@ namespace Products
             e.Graphics.DrawLine(pen, 15, 215, 580, 215);
             e.Graphics.DrawLine(pen, 15, 257, 580, 257);
         }
+
         // Выход из приложения.
         private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+
         // Проверка ввода целых чисел.
         private void tbCipher_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -61,7 +61,6 @@ namespace Products
                     e.Handled = true;
                 }
             }
-
         }
 
         private void Filling(string commandText)
@@ -72,7 +71,7 @@ namespace Products
                 SqlCommand thisCommand = connection.CreateCommand();
                 thisCommand.CommandText = commandText;
                 SqlDataReader thisReader = thisCommand.ExecuteReader();
-                // Заполнение ТБ на форме. 
+                // Заполнение ТБ на форме.
                 while (thisReader.Read())
                 {
                     tbCipher.Text = thisReader["Id"].ToString();
@@ -86,10 +85,12 @@ namespace Products
                 thisReader.Close();
                 connection.Close();
             }
+            cmbGenre.SelectedIndex = Convert.ToInt32(tbGenre.Text) - 1;
         }
+
         private void btnFirst_Click(object sender, EventArgs e)
         {
-            string commandText = @"SELECT Top 1 * 
+            string commandText = @"SELECT Top 1 *
                                    FROM Games
                                    ORDER BY id";
             Filling(commandText);
@@ -114,7 +115,7 @@ namespace Products
             // Подтверждение удаления.
             DialogResult dialogResult = MessageBox.Show($"Вы уверены, что желаете удалить товар с номером {tbCipher.Text}?",
                 "Предупреждение",
-                MessageBoxButtons.YesNo, 
+                MessageBoxButtons.YesNo,
                 MessageBoxIcon.Warning);
             if (dialogResult == DialogResult.Yes)
             {
@@ -137,7 +138,6 @@ namespace Products
 
             string sqlExpression = "SELECT Games.Id, Games.Name, GenreId, Genres.Name, Type, Quantity, Price, Allowance FROM Games, Genres WHERE Genres.Id = GenreId ORDER BY Id DESC";
             string sqlExpression2 = "SELECT * FROM Genres";
-            Filling(sqlExpression);
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -151,8 +151,9 @@ namespace Products
                     }
                 }
                 reader.Close();
-                cmbGenre.SelectedIndex = Convert.ToInt32(tbGenre.Text) - 1;
             }
+            // Заполняем ТБ.
+            Filling(sqlExpression);
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
@@ -168,7 +169,7 @@ namespace Products
                     reader.Close();
                     DialogResult dialogResult = MessageBox.Show($"Точно изменить запись {tbCipher.Text}?", "Предупреждение", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
                     if (dialogResult == DialogResult.Yes)
-                    {    
+                    {
                         string sqlExpression2 = $"UPDATE Games SET Name='{tbName.Text}', GenreId={tbGenre.Text}, Type='{tbType.Text}', Quantity={tblNumber.Text}, Price={tbPrice.Text}, Allowance={tbSurcharge.Text} WHERE Games.Id='{tbCipher.Text}'";
                         command = new SqlCommand(sqlExpression2, connection);
                         command.ExecuteNonQuery();
@@ -204,7 +205,7 @@ namespace Products
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            string commandText = $@"SELECT * 
+            string commandText = $@"SELECT *
                                    FROM Games
                                    WHERE Id > {int.Parse(tbCipher.Text)} ORDER BY Id DESC";
             Filling(commandText);
@@ -212,7 +213,7 @@ namespace Products
 
         private void btnBefore_Click(object sender, EventArgs e)
         {
-            string commandText = $@"SELECT * 
+            string commandText = $@"SELECT *
                                    FROM Games
                                    WHERE Id < {int.Parse(tbCipher.Text)} ORDER BY Id";
             Filling(commandText);
@@ -226,6 +227,7 @@ namespace Products
                 e.Handled = true;
             }
         }
+
         private void cmbGenre_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cmbGenre.Text == "Action")
